e9f3c29af644de66a55e26ea922453a7a0a6f6fc
zsline <sergei.dereka@gmail.com>
2023-06-27 18:35:56 +0300

    first commit

---
+++ src/js/modules/functions.js
@@ -0,0 +1,27 @@
+/* Проверка поддержки webp, добавление класса для html */
+export function isWebp() {
+    //проверка поддержки webp
+    function testWebP(callback) {
+        let webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    //добавление класса _webp или _no-webp для HTML
+    testWebP(function (support) {
+        let className = support === true ? 'webp' : 'no-webp';
+        document.documentElement.classList.add(className);
+    });
+}
+
+// start animation
+
+const blockAnim = document.querySelectorAll('.animate');
+if(blockAnim){
+    blockAnim.forEach((el) => {
+        el.addEventListener('click', function() {
+            el.classList.toggle('anim-start');
+        });
+    })
+}
